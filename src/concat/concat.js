2014957ad73bc78391ce97368a37ce3862e5cc45
Muslih <muslihzarth@gmail.com>
2017-05-18 22:00:24 +0800

    styling page in content and footer

--- src/concat/concat.js
+++ src/concat/concat.js
@@ -12634,5 +12634,15 @@ $(document).ready(function(){
 	tagline = $('.tagline')
 	tagline.css('background-image', 'url(' + tagline.data('bg') + ')');
 	tagline.css('background-position', tagline.data('position'));
+
+
+	head = $('header');
+    return $(window).scroll(function() {
+      if ($(this).scrollTop() > 100) {
+        return head.addClass('fill');
+      } else {
+        return head.removeClass('fill');
+      }
+    });
 })
 console.log('@muslihzarth');
