a33ce600f2cb0c45d0f38c06aa6e6c58b19fb171
Muslih <muslihzarth@gmail.com>
2017-05-19 11:29:58 +0800

    add about page

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
 	tagline = $('.tagline')
 	tagline.css('background-image', 'url(' + tagline.data('bg') + ')');
 	tagline.css('background-position', tagline.data('position'));
+	if (tagline.data('size')) {
+  	tagline.css('background-size', tagline.data('size'));
+	}
 
 
 	head = $('header');
